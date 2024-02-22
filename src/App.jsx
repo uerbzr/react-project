a59ba9b8795a4ef3f947223a6f66c41343dcf7c9
uerbzr <nigel.sibbert@gmail.com>
2024-02-22 08:26:58 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CounterComponent from './components/CounterComponent'
+import CarComponent from './components/CarComponent'
+function App() {
+  
+
+  return (
+    <>
+      <CarComponent />
+      <hr />
+      <CarComponent />
+      <hr />
+      <CounterComponent />
+      <hr />
+      <CounterComponent />
+    </>
+  )
+}
+
+export default App
