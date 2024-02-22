ced2edb2010432680c57ec1720c8940c9764c374
uerbzr <nigel.sibbert@gmail.com>
2024-02-22 11:18:05 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,53 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import CounterComponent from './components/CounterComponent'
 import CarComponent from './components/CarComponent'
+import { useState } from 'react'
+import VehicleComponent from './components/VehicleComponent'
 function App() {
   
 
+    const [bikes, setBikes] = useState([
+    {
+        id:1,
+        make:"KTM",
+        model:"390 Adventure"
+    },
+    {
+        id:2,
+        make:"Yamaha",
+        model:"XT660Z Tenere"
+    },
+    {
+        id:3,
+        make:"BMW",
+        model:"F650"
+    }
+])
+
+
+  const [cars, setCars] = useState([
+      {
+          id:1,
+          make:"Mini",
+          model:"Clubman"
+      },
+      {
+          id:2,
+          make:"Mini",
+          model:"One"
+      },
+      {
+          id:3,
+          make:"Smart",
+          model:"ForTwo"
+      }
+  ])
+
   return (
     <>
+      <VehicleComponent title={"Cars"} vehicles={cars}/>
+      <VehicleComponent title={"Bikes"} vehicles={bikes}/>
+      <hr />
       <CarComponent />
       <hr />
       <CarComponent />
